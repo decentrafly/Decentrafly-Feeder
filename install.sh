4b3b873aa4815bc55e27e79a9b1941d778921ed4
Constantin Michael Weisser <michael@decentrafly.com>
2023-04-22 14:42:44 -0300

    Update install script

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ Missing  Install command
 curl     sudo apt-get install curl
 env      sudo apt-get install coreutils
 mktemp   sudo apt-get install coreutils
-pip3     sudo apt-get install python3-pip
 python3  sudo apt-get install python3
 
 HERE
@@ -30,19 +29,16 @@ function check_dependency() {
 check_dependency curl
 check_dependency env
 check_dependency mktemp
-check_dependency pip3
 check_dependency python3
 
 cd "$(mktemp -d)"
 
-curl -L 'https://github.com/decentrafly/MQTT/releases/download/v2023-04-18_2/decentrafly' > decentrafly
+curl -L 'https://github.com/decentrafly/MQTT/releases/download/v2023-04-22/decentrafly' > decentrafly
 echo "Checking file integrity"
-echo '732191eba5a15afaf8b3318f23ebdab04e3207c7  decentrafly' | sha1sum --check
+echo 'c43e77df6944b47e15d26695f61091e52efb8626  decentrafly' | sha1sum --check
 
 chmod 777 decentrafly
 
-echo "Installing dependencies ..."
-pip3 install awscrt==0.16.13 awsiot==0.1.3 awsiotsdk==1.12.6 boto3==1.26.87 requests==2.28.2
-
 ./decentrafly setup
+./decentrafly install
 ./decentrafly enable
