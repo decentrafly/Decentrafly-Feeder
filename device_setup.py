dbd60997bc2e84439f7978d655cbcef8a8d3d872
Constantin Michael Weisser <michael@decentrafly.com>
2023-04-27 22:20:25 -0300

    Submit attribute values about the sender

--- device_setup.py
+++ device_setup.py
@@ -32,7 +32,7 @@ def client_id():
     return "beastdatafeed-" + str(uuid.uuid4())
 
 
-def register_new_device(temporary_dir):
+def register_new_device(temporary_dir, device_attributes):
     thing_name = client_id()
     config_content = {"DCF_CLIENT_ID": thing_name,
                       "DCF_MQTT_HOST": "almfwrqpajske-ats.iot.us-east-1.amazonaws.com",
@@ -41,7 +41,8 @@ def register_new_device(temporary_dir):
     aws_root_ca = requests.request('GET', "https://www.amazontrust.com/repository/AmazonRootCA1.pem")
     response = requests.request('POST',
                                 "https://decentrafly.org/api/device/register",
-                                json={"thing_name": thing_name})
+                                json={"thing_name": thing_name,
+                                      "attributes": device_attributes})
     data = response.json()
     write_to_file(os.path.join(temporary_dir, "cert.crt"), data['certs']['certificate'])
     write_to_file(os.path.join(temporary_dir, "private.key"), data['certs']['private_key'])
@@ -50,15 +51,49 @@ def register_new_device(temporary_dir):
                   json.dumps(config_content, indent=2))
 
 
+def parse_config_file(file_path):
+    config_dict = {}
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    key, value = line.split('=')
+                    config_dict[key.strip()] = value.strip()
+    return config_dict
+
+
+def detect_device_attributes():
+    adsb_config = parse_config_file("/boot/adsb-config.txt")
+    required_keys = ['LATITUDE', 'LONGITUDE', 'USER', 'ALTITUDE']
+    user_friendly_name = {
+        "ALTITUDE": "the altitude of your antenna",
+        "LATITUDE": "the latitude of your geo position",
+        "LONGITUDE": "the longitude of your geo position",
+        "USER": "your device nickname"
+        }
+
+    if any(key not in adsb_config for key in required_keys):
+        print("\nSome information about your device could not be detected automatically")
+        print("We kindly ask to provide this information, but you can leave it empty if you prefer.\n")
+
+    for key in required_keys:
+        if key not in adsb_config:
+            value = input(f"Please provide a value for {user_friendly_name[key]}: ")
+            adsb_config[key] = value
+    return adsb_config
+
+
 def self_setup():
     if not (os.path.isfile(os.path.expanduser("/etc/decentrafly/ca.crt"))
             and os.path.isfile(os.path.expanduser("/etc/decentrafly/cert.crt"))
             and os.path.isfile(os.path.expanduser("/etc/decentrafly/private.key"))):
-        print("Please provide the sudo password to write config files to /etc/decentrafly")
+        device_attributes = detect_device_attributes()
+        print("\n\nPlease provide the sudo password to write config files to /etc/decentrafly")
         exit_code = 0
         exit_code += subprocess.call(['sudo', 'echo'])
         with tempfile.TemporaryDirectory() as tmpdirname:
-            register_new_device(tmpdirname)
+            register_new_device(tmpdirname, device_attributes)
             exit_code += subprocess.call(['sudo', 'cp', '-r', tmpdirname, "/etc/decentrafly"])
             exit_code += subprocess.call(['sudo', 'chmod', '755', tmpdirname, "/etc/decentrafly"])
         if exit_code == 0:
