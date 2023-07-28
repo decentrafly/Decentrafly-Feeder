9a40cd9ab4212ffa478f34bf89d446c1f69cae91
Constantin Michael Weisser <michael@decentrafly.com>
2023-07-27 21:59:47 -0300

    Move config out of sender

---
+++ config.py
@@ -0,0 +1,30 @@
+import json
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+def load_config():
+    config_file_path = os.path.expanduser("/etc/decentrafly/config.json")
+    if os.path.isfile(config_file_path):
+        config = json.load(open(config_file_path))
+        return config
+    else:
+        return {}
+
+
+default_config = {
+    "DCF_CLIENT_ID": "notset",
+    "DCF_IOT_TOPIC": "beast/ingest/0",
+    "DCF_LOG_INTERVAL": "20",
+    "DCF_MAX_INTERVAL": "1.0",
+    "DCF_MAX_MESSAGE_SIZE": "100000",
+    "DCF_READSB_HOST": "localhost",
+    "DCF_READSB_PORT": "30005",
+    "DCF_TRIGGER_SIZE": "50000",
+    }
+
+environment_config = {k: os.getenv(k) for k in default_config.keys()
+                      if os.getenv(k) is not None}
+
+effective_config = {**default_config, **load_config(), **environment_config}
--- sender.py
+++ sender.py
@@ -1,4 +1,5 @@
 from awscrt import mqtt
+from config import effective_config
 import beast
 import functools
 import json
@@ -10,34 +11,7 @@ import socket
 import subprocess
 import time
 
-logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger("sender")
-
-
-def load_config():
-    config_file_path = os.path.expanduser("/etc/decentrafly/config.json")
-    if os.path.isfile(config_file_path):
-        config = json.load(open(config_file_path))
-        return config
-    else:
-        return {}
-
-
-default_config = {
-    "DCF_CLIENT_ID": "notset",
-    "DCF_IOT_TOPIC": "beast/ingest/0",
-    "DCF_LOG_INTERVAL": "20",
-    "DCF_MAX_INTERVAL": "1.0",
-    "DCF_MAX_MESSAGE_SIZE": "100000",
-    "DCF_READSB_HOST": "localhost",
-    "DCF_READSB_PORT": "30005",
-    "DCF_TRIGGER_SIZE": "50000",
-    }
-
-environment_config = {k: os.getenv(k) for k in default_config.keys()
-                      if os.getenv(k) is not None}
-
-effective_config = {**default_config, **load_config(), **environment_config}
 
 aws_iot_topic = effective_config['DCF_IOT_TOPIC']
 client_id = effective_config['DCF_CLIENT_ID']
