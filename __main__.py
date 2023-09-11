b76f155b19075468105cf44b68098aa2694c441e
Constantin Michael Weisser <michael@decentrafly.com>
2023-09-11 13:57:01 -0300

    Use new endpoints for setup

--- __main__.py
+++ __main__.py
@@ -68,5 +68,6 @@ def main():
 
     log.debug("Terminating")
 
+
 if __name__ == '__main__':
     main()
--- device_setup.py
+++ device_setup.py
@@ -119,18 +119,17 @@ def register_new_device(temporary_dir, device_attributes):
     aws_root_ca = requests.request('GET', "https://www.amazontrust.com/repository/AmazonRootCA1.pem")
     print("Generating a device ID and new certificates ...")
     response = requests.request('POST',
-                                "https://decentrafly.org/api/device/register/v2",
+                                "https://decentrafly.org/api/devices/new",
                                 json={"attributes": device_attributes})
     data = response.json()
-    write_to_file(os.path.join(temporary_dir, "cert.crt"), data['mqtt']['certs']['certificate'])
-    write_to_file(os.path.join(temporary_dir, "private.key"), data['mqtt']['certs']['private_key'])
+    write_to_file(os.path.join(temporary_dir, "cert.crt"), data['agent_certs']['cert'])
+    write_to_file(os.path.join(temporary_dir, "private.key"), data['agent_certs']['private_key'])
     write_to_file(os.path.join(temporary_dir, "ca.crt"), aws_root_ca.text)
-    write_to_file(os.path.join(temporary_dir, "mtls-cert.crt"), data['mtls']['certificate'])
-    write_to_file(os.path.join(temporary_dir, "mtls-private.key"), data['mtls']['private_key'])
-    write_to_file(os.path.join(temporary_dir, "mtls-ca.crt"), data['mtls']['ca'])
+    write_to_file(os.path.join(temporary_dir, "mtls-cert.crt"), data['tls_certs']['cert'])
+    write_to_file(os.path.join(temporary_dir, "mtls-private.key"), data['tls_certs']['private_key'])
+    write_to_file(os.path.join(temporary_dir, "mtls-ca.crt"), data['tls_certs']['ca'])
 
-    config_content = {"DCF_FEEDER_ID": data['feeder_id'],
-                      "DCF_CLIENT_ID": data['mqtt']['thing']['name'],
+    config_content = {"DCF_FEEDER_ID": data['device_id'],
                       "DCF_MQTT_HOST": "almfwrqpajske-ats.iot.us-east-1.amazonaws.com",
                       "DCF_IOT_TOPIC": "beast/ingest/" + str(randrange(1, 100))}
     write_to_file(os.path.join(temporary_dir, "config.json"),
@@ -202,13 +201,13 @@ def update_iot_device():
     # the same run as the self_setup. So config.effective_config is
     # not up to date as currently it's only loaded on startup
     up_to_date_config = config.load_config()
-    if up_to_date_config['DCF_CLIENT_ID']:
+    if up_to_date_config['DCF_FEEDER_ID']:
         print("Updating device configuration on decentrafly backend ...")
-        requests.request(
-            'POST',
-            "https://decentrafly.org/api/device/update",
-            json={"thing_name": up_to_date_config['DCF_CLIENT_ID']}
+        response = requests.request(
+            'PUT',
+            "https://decentrafly.org/api/device/{}/update".format(up_to_date_config['DCF_FEEDER_ID'])
         )
+        print(response)
     else:
         print("Update iot device failed.")
 
