d8b396869ad264b8657bd644f0c60f3b01744b2d
Constantin Michael Weisser <michael@decentrafly.com>
2023-09-11 19:45:03 -0300

    Bump install script

--- install.sh
+++ install.sh
@@ -35,9 +35,9 @@ check_dependency python3
 
 cd "$(mktemp -d)"
 
-curl -L 'https://github.com/decentrafly/MQTT-Feeder/releases/download/v2023-09-11_2/decentrafly' > decentrafly
+curl -L 'https://github.com/decentrafly/MQTT-Feeder/releases/download/v2023-09-11_3/decentrafly' > decentrafly
 echo "Checking file integrity"
-echo 'fe1badb49816655b9b79d6813a42536a3d137abc  decentrafly' | sha1sum --check
+echo '888ffcaeca78b75f16e0afee19606a4ffef6d7c5  decentrafly' | sha1sum --check
 
 
 chmod 777 decentrafly
